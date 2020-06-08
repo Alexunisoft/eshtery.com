33892408bdeb24d8e0838b2c35f30edb387d05ba
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-08 22:19:41 +0200

    craete categorySchema in the category schema file

---
+++ Database/Schema/CategorySchema.js
@@ -0,0 +1,21 @@
+const schema = require('mongoose').Schema;
+/**
+ * define schema for Category collection
+ * that has three fields for each document 
+ * name , created_at , updated_at
+ */
+const categorySchema = new schema({
+    name: {
+        type: String,
+        required: [true,
+            "Category name is required",
+        ]
+    },
+    created_at: {
+        type: Date,
+        required: true
+    },
+    updated_at: {
+        type: Date,
+    }
+})
