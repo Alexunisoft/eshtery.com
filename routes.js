b5034a9579df70e0d7f21de33009e447b5ca62c9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-09 01:52:59 +0200

    add middleware for body-pareser

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const UsersController = require("./Users/UsersController").UsersController;
 const CategoriesController = require("./Categories/CategoriesController").CategoryController;
+const bodyParser = require('body-parser');
 module.exports = function(app) {
+    app.use(bodyParser.urlencoded({ extended: false }));
     app.use("/users", UsersController);
     app.use("/categories", CategoriesController);
 };
