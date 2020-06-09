8af0d2e67295948340793a3668cfb5cd7faa664f
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-09 14:04:49 +0200

    create index method for Categories

--- Categories/CategoriesController.js
+++ Categories/CategoriesController.js
@@ -4,11 +4,23 @@ const CategoryModel = require('./Category');
  * Categories controller
  */
 /**
- * 
+ * create index function to get all Categories available in DB
  */
 CategoryController.get('/', function index(req, res) {
 
-    res.send("hello from category controller");
+    CategoryModel.find({}, (err, data) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else if (data.length == 0) {
+            res.status(204); //204 to say that the server worked successfuly but there is no content
+            res.json(data);
+        } else {
+            res.status(200);
+            res.json(data);
+        }
+    });
+
 });
 /**
  * create store function to create new document from category model
