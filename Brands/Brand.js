6b655864b8a961c8627c5768be152155b55758d5
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-09 22:15:48 +0200

    create brand model from brandSchema

---
+++ Brands/Brand.js
@@ -0,0 +1,11 @@
+const BrandSchema = require('../Database/Schema/BrandSchema').BrandSchema;
+const model = require('mongoose').model;
+const autoIncrement = require('mongoose-auto-increment');
+/**
+ * Mongoose plugin that auto-increments any ID field on your schema every time a document is saved.
+ */
+BrandSchema.plugin(autoIncrement.plugin, { model: 'Brand', startAt: 1 });
+/**
+ * create model for Brand from BrandSchema
+ */
+module.exports.Brand = model('Brand', BrandSchema);
