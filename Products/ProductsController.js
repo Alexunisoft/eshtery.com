af5eedc599142f0553f9397676de97ed791d20b3
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-10 17:24:59 +0200

    create store function for product controller

---
+++ Products/ProductsController.js
@@ -0,0 +1,28 @@
+const ProductsController = require('express').Router();
+const ProductModel = require('./Product').ProductModel;
+/**
+ * ProductsController
+ */
+/**
+ * create store function to create new product document in Product Collection in DB
+ */
+ProductsController.get('/', function store(req, res) {
+    let product = new ProductModel({
+        'name': req.body.name,
+        'description': req.body.description,
+        'price': req.body.price,
+        'brand_id': req.body.brand_id,
+        'category_id': req.body.category_id,
+    });
+    product.save((err) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else {
+            res.status(201);
+            res.json({
+                "status": "Product created successfully "
+            });
+        }
+    });
+})
