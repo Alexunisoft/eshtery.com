6b2091d19e1047a6bef68189c4ba450e616ac7dc
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-10 13:45:38 +0200

    add Brands controller in routes

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const UsersController = require("./Users/UsersController").UsersController;
 const CategoriesController = require("./Categories/CategoriesController").CategoryController;
+const BrandsController = require('./Brands/BrandsController')
 const bodyParser = require('body-parser');
 module.exports = function(app) {
     app.use(bodyParser.json({}));
     app.use("/users", UsersController);
     app.use("/categories", CategoriesController);
+    app.use("brands", BrandsController);
 };
