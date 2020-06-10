5ff8713f022ca8f10b25cd9ae8eb235b48409468
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-10 19:09:25 +0200

    create show method for product controller

--- Products/ProductsController.js
+++ Products/ProductsController.js
@@ -26,26 +26,45 @@ ProductsController.get('/', function index(req, res) {
  * create store function to create new product document in Product Collection in DB
  */
 ProductsController.post('/', function store(req, res) {
-        let product = new ProductModel({
-            'name': req.body.name,
-            'description': req.body.description,
-            'price': req.body.price,
-            'brand': req.body.brand,
-            'category': req.body.category,
-        });
-        product.save((err) => {
-            if (err) {
-                res.status(500);
-                res.json(err);
-            } else {
-                res.status(201);
-                res.json({
-                    "status": "Product created successfully "
-                });
-            }
-        });
-    })
-    /**
-     * export ProductsController
-     */
+    let product = new ProductModel({
+        'name': req.body.name,
+        'description': req.body.description,
+        'price': req.body.price,
+        'brand': req.body.brand,
+        'category': req.body.category,
+    });
+    product.save((err) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else {
+            res.status(201);
+            res.json({
+                "status": "Product created successfully "
+            });
+        }
+    });
+});
+/**
+ * create show function to get Product document
+ */
+ProductsController.get('/:id', function show(req, res) {
+    ProductModel.findById({ _id: req.params.id }, ).populate(['brand', 'category']).exec((err, data) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else if (data) {
+            res.status(200);
+            res.json(data);
+        } else {
+            res.status(404);
+            res.json({
+                "status": "Resourse Not found "
+            });
+        }
+    });
+});
+/**
+ * export ProductsController
+ */
 module.exports.ProductsController = ProductsController;
