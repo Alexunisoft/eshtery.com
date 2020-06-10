39f2bd0474908ae36212fdecfbe2645bdb47add9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-10 14:43:48 +0200

    create index function for brand controller

--- Brands/BrandsController.js
+++ Brands/BrandsController.js
@@ -4,10 +4,23 @@ const BrandModel = require('./Brand');
  * Brand Controller
  */
 /**
- * 
+ * create index function to get all documents in Brands Collection
  */
-BrandController.get('/', function index(req, rea) {
-
+BrandController.get('/', function index(req, res) {
+    BrandModel.find({}, (err, data) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else if (data) {
+            res.status(200);
+            res.json(data);
+        } else {
+            res.status(404);
+            res.json({
+                "status": "Resourse Not found "
+            })
+        }
+    })
 });
 /**
  * store function to create new Brand document in DB
