ac165ce8bfc7f10740b53bcd9ef00d37bbfa4f57
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-10 18:41:48 +0200

    create store function for Product controller

--- Products/ProductsController.js
+++ Products/ProductsController.js
@@ -6,23 +6,27 @@ const ProductModel = require('./Product').ProductModel;
 /**
  * create store function to create new product document in Product Collection in DB
  */
-ProductsController.get('/', function store(req, res) {
-    let product = new ProductModel({
-        'name': req.body.name,
-        'description': req.body.description,
-        'price': req.body.price,
-        'brand_id': req.body.brand_id,
-        'category_id': req.body.category_id,
-    });
-    product.save((err) => {
-        if (err) {
-            res.status(500);
-            res.json(err);
-        } else {
-            res.status(201);
-            res.json({
-                "status": "Product created successfully "
-            });
-        }
-    });
-})
+ProductsController.post('/', function store(req, res) {
+        let product = new ProductModel({
+            'name': req.body.name,
+            'description': req.body.description,
+            'price': req.body.price,
+            'brand_id': req.body.brand_id,
+            'category_id': req.body.category_id,
+        });
+        product.save((err) => {
+            if (err) {
+                res.status(500);
+                res.json(err);
+            } else {
+                res.status(201);
+                res.json({
+                    "status": "Product created successfully "
+                });
+            }
+        });
+    })
+    /**
+     * export ProductsController
+     */
+module.exports.ProductsController = ProductsController;
