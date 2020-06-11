a3664aa120da9b9da87924dabe490105e973b63e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-11 18:35:02 +0200

    use  mongoose autoincrement in user schema

--- Database/Schema/UserSchema.js
+++ Database/Schema/UserSchema.js
@@ -1,5 +1,5 @@
 const schema = require("mongoose").Schema;
-
+const autoIncrement = require('mongoose-auto-increment');
 /**
  * UserSchema Collection definition
  */
@@ -9,5 +9,9 @@ const UserSchema = new schema({
     "created_at": Date,
     "updated_at": Date,
 });
+/**
+ * Mongoose plugin that auto-increments any ID field on your schema every time a document is saved.
+ */
+UserSchema.plugin(autoIncrement.plugin, { model: 'User', startAt: 1 });
 
 module.exports.UserSchema = UserSchema;
