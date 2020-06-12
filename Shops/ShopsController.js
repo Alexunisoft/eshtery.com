e18525b2305cab929203d35f4c64007f62079d78
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-06-12 17:04:17 +0200

    create index method for ShopController

--- Shops/ShopsController.js
+++ Shops/ShopsController.js
@@ -1,8 +1,30 @@
+const { json } = require('body-parser');
+
 const ShopsController = require('express').Router();
 const ShopModel = require('./Shop').ShopModel;
 /**
  * Shop Controller
  */
+/**
+ * create Index function to get collection of Shops
+ */
+ShopsController.get('/', function index(req, res) {
+    ShopModel.find({}).populate('user').exec((err, data) => {
+        if (err) {
+            res.status(500);
+            res.json(err);
+        } else if (data) {
+            res.status(200);
+            res.json(data);
+        } else {
+            res.status(404);
+            res.json({
+                "status": "Resourse Not found "
+            });
+        }
+    });
+
+});
 /**
  * create Store function to create new document of Shop
  */
