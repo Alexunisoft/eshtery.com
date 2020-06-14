61f4a4a9cb89dd287eb62eada2ff176e8cbceae3
shimaa.hosny.aboelhassan@gmail.com <shimaa.hosny.aboelhassan@gmail.com>
2020-06-14 21:54:59 +0200

    fix file doc block

--- Middleware/Middleware.js
+++ Middleware/Middleware.js
@@ -1,7 +1,8 @@
-/**-----------------------------------------------------
+/**-------------------------------------------------------------------------------
  * All middleware declarations of the application are defined in this file.
  * Feel free to add extra middleware declarations when you add a new middleware 
  * definition to the app. 
+ * -------------------------------------------------------------------------------
  */
 const bodyParser = require('body-parser');
 const expressSession = require("express-session");
