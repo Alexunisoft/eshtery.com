c7124585031b3dfb70392cb5ffe19144ff3fe26c
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2020-06-19 13:05:38 +0200

    rectify route to be redirected to on failed authentication

--- Middleware/RouteMiddleware/auth.js
+++ Middleware/RouteMiddleware/auth.js
@@ -8,7 +8,7 @@ function auth(req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.redirect("/auth/login");
+        res.redirect("/auth/form");
     }
 }
 
