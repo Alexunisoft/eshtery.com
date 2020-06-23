103a4971fff675dee1175ae4d160827a15f950ec
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2020-06-23 20:55:21 +0200

    rectify index.js structure

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const bootstrap = require("./bootstrap");
 const express = require("express");
 const app = express();
 
-bootstrap(app).then(function(){
+bootstrap(app).then(function () {
     app.listen(env("PORT"), function () {
         console.log("Server started listening on port", env("PORT"));
     });
 
-}).catch(function(err){
+}).catch(function (err) {
     console.log("something went wrong");
     console.error(err);
 });
